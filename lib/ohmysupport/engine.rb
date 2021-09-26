6687c212cca3acf9fc15cd32c4799fdf7997ec76
Stanislav Mekhonoshin <ejabberd@gmail.com>
2021-09-26 20:54:18 +0300

    Add bootstrap styling

--- lib/ohmysupport/engine.rb
+++ lib/ohmysupport/engine.rb
@@ -1,5 +1,9 @@
 module Ohmysupport
   class Engine < ::Rails::Engine
     isolate_namespace Ohmysupport
+
+    initializer "ohmysupport.assets.precompile" do |app|
+      app.config.assets.precompile += %w( ohmysupport/application.css )
+    end
   end
 end
